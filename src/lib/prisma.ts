d44d1085866fcf050b8050a77e384591c8b7ab99
Reza Fakhor <rezafakhor@gmail.com>
2026-02-13 18:07:05 +0700

    Add pgbouncer parameter to fix prepared statement error

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -6,6 +6,11 @@ const globalForPrisma = globalThis as unknown as {
 
 export const prisma = globalForPrisma.prisma ?? new PrismaClient({
   log: process.env.NODE_ENV === 'development' ? ['error', 'warn'] : ['error'],
+  datasources: {
+    db: {
+      url: process.env.POSTGRES_URL + '?pgbouncer=true&connect_timeout=15'
+    }
+  }
 });
 
 if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma;

95ef746c461b410555fffe6570c95d61654f554f
Reza Fakhor <rezafakhor@gmail.com>
2026-02-13 22:19:37 +0700

    Add Supabase domain to Next.js image config

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'aldfhmhqvpyozfiwserl.supabase.co',
+        pathname: '/storage/v1/object/public/**',
+      },
+    ],
     domains: ['localhost'],
   },
 };
